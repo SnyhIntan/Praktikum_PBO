e6a8f032d85ad7e3ce1680e80ef47a541da01477
SnyhIntan <162134309+SnyhIntan@users.noreply.github.com>
2025-04-21 19:59:06 +0800

    Add files via upload

---
+++ posttest5/src/Dosen.java
@@ -0,0 +1,41 @@
+public class Dosen extends User {
+    private String departemen;
+
+    public Dosen(String nip, String password, String nama, String email, String departemen) {
+        super(nip, password, nama, email);
+        this.departemen = departemen;
+    }
+
+    public String getDepartemen() {
+        return departemen;
+    }
+
+    public void setDepartemen(String departemen) {
+        this.departemen = departemen;
+    }
+
+    public void ajukanPeminjaman(String kodeRuangan, String tanggal, String waktuMulai, String waktuSelesai) {
+        System.out.println("\n\nDosen " + nama + " berhasil mengajukan peminjaman ruangan!!");
+        System.out.println("Ruangan : " + kodeRuangan);
+        System.out.println("Tanggal : " + tanggal);
+        System.out.println("Waktu   : " + waktuMulai + " - " + waktuSelesai);
+        System.out.print("\nHarap ruangan digunakan dengan baik.");
+        System.out.print("\nTekan Enter untuk kembali...");
+        scanner.nextLine();
+    }
+
+    @Override
+        public void tampilkanInfo() {
+        System.out.println("\nSelamat datang, Dosen " + getNama() + "!");
+        System.out.println("======================================");
+        System.out.println("NIP        : " + getNimNip());
+        System.out.println("Email      : " + getEmail());
+        System.out.println("Departemen : " + departemen);
+        System.out.println("======================================\n");
+    }
+
+    @Override
+    public String toString() {
+        return "Dosen - " + super.toString() + ", Departemen: " + departemen;
+    }
+}
---
+++ posttest5/src/Mahasiswa.java
@@ -0,0 +1,42 @@
+public class Mahasiswa extends User {
+    private String prodi;
+
+    public Mahasiswa(String nim, String password, String nama, String email, String prodi) {
+        super(nim, password, nama, email);
+        this.prodi = prodi;
+    }
+
+    public String getProdi() {
+        return prodi;
+    }
+
+    public void setProdi(String prodi) {
+        this.prodi = prodi;
+    }
+
+    public void ajukanPeminjaman(String kodeRuangan, String tanggal, String waktuMulai, String waktuSelesai) {
+        System.out.println("\n\nMahasiswa " + nama + " berhasil mengajukan peminjaman ruangan!!");
+        System.out.println("Ruangan : " + kodeRuangan);
+        System.out.println("Tanggal : " + tanggal);
+        System.out.println("Waktu   : " + waktuMulai + " - " + waktuSelesai);
+        System.out.print("\nHarap ruangan digunakan dengan baik.");
+        System.out.print("\nTekan Enter untuk kembali...");
+        scanner.nextLine();
+    }
+    
+    @Override
+    public void tampilkanInfo() {
+        System.out.println("\nSelamat datang, Mahasiswa " + getNama() + "!");
+        System.out.println("======================================");
+        System.out.println("NIM   : " + getNimNip());
+        System.out.println("Email : " + getEmail());
+        System.out.println("Prodi : " + prodi);
+        System.out.println("======================================\n");
+    }
+    
+
+    @Override
+    public String toString() {
+        return "Mahasiswa - " + super.toString() + ", Prodi: " + prodi;
+    }
+}
